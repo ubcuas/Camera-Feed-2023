bfb32521a8360b7f3923d0e21cf0732054c59da3
Richard Han <rrhan2002@gmail.com>
2024-03-16 15:03:53 -0700

    no message

--- camera_feed_C/src/curltest.cpp
+++ camera_feed_C/src/curltest.cpp
@@ -3,17 +3,25 @@
 
 int main() {
     CURL *curl;
-    CURLcode res;
+    CURLcode result;
     
     curl = curl_easy_init();
 
-    // if (curl == NULL) {
-    //     fprintf(stderr, "HTTP request failed");
-    //     return -1;
-    // }
+    if (curl == NULL) {
+        fprintf(stderr, "HTTP request failed");
+        return -1;
+    }
 
-    // curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
 
+    result = curl_easy_perform(curl);
+
+    if (result != CURLE_OK) {
+        fprintf(stderr, "Error: %s\n", curl_easy_strerror(result));
+        return -1;
+    }
+    
+    curl_easy_cleanup(curl);
 
 
     return 0;
