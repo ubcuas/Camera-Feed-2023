d75844eae4128015d6f6f16fb6d2b68bd7e0e0d6
Richard Han <rrhan2002@gmail.com>
2024-06-25 17:49:04 -0400

    fixed

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import os
+import shutil
+
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 import uvicorn
+
+
 app = FastAPI()
 
 
@@ -8,9 +14,21 @@ async def feed():
     return {"msg": "hello world"}
 
 
-@app.post("/feed")
-async def feed(file: UploadFile):
-    return {"file_name": file.filename}
+@app.post("/image")
+async def upload_file(image: UploadFile = File(...)):
+    try:
+        # change directory as preferred
+        directory = "./imgs"
+        os.makedirs(directory, exist_ok=True)
+
+        # Save the file to a specified location
+        with open(os.path.join(directory, image.filename), "wb") as buffer:
+            shutil.copyfileobj(image.file, buffer)
+
+        return JSONResponse(status_code=200, content={"message": "File uploaded successfully"})
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": f"Could not upload file. Error: {str(e)}"})
 
 
 if __name__ == "__main__":
