9deaab5489232bf98e84e7955325a78edba16ed5
Richard Han <rrhan2002@gmail.com>
2024-11-02 15:19:01 -0700

    compiles, start pipeline refactor

--- src/HttpTransmitter.cpp
+++ src/HttpTransmitter.cpp
@@ -13,6 +13,9 @@
 
 #include <opencv2/opencv.hpp>
 
+#include "Pipeline.hpp"
+
+
 #define MAX_ATTEMPTS 100
 
 // Callback function to handle the response
@@ -140,65 +143,38 @@ bool HttpTransmitter::send_imgfile(const std::string &url,
 }
 
 bool HttpTransmitter::send_imen(const std::string &url,
-                                std::unique_ptr<std::vector<uchar>> buf_ptr,
-                                int64_t timestamp) {
+                                std::unique_ptr<EncodedData> encoded) {
   CURLcode res;
   char error[CURL_ERROR_SIZE];
   curl_mime *form = NULL;
   curl_mimepart *field = NULL;
-  std::string timestamp_str = std::to_string(timestamp) + ".jpg";
+  std::string timestamp_str = std::to_string(encoded->timestamp) + ".jpg";
 
   curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
   curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, error);
 
-  int attempts = 0;
-  while ((form = curl_mime_init(curl)) == NULL && attempts < MAX_ATTEMPTS) {
-    fprintf(stderr,
-            "curl_mime_init() failed, attempt %d of %d: %s\n",
-            attempts + 1,
-            MAX_ATTEMPTS,
-            error);
-    attempts++;
-  }
+  form = curl_mime_init(curl);
 
   if (form == NULL) {
-    fprintf(stderr, "Maximum attempts exceeded, skipping\n");
+    fprintf(stderr, "curl_mime_init() failed\n");
     return false;
   }
   curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
-
-  attempts = 0;
-  while ((field = curl_mime_addpart(form)) == NULL && attempts < MAX_ATTEMPTS) {
-    fprintf(stderr,
-            "curl_mime_addpart() failed, attempt %d of %d: %s\n",
-            attempts + 1,
-            MAX_ATTEMPTS,
-            error);
-    attempts++;
+  field = curl_mime_addpart(form);
+  if (field == NULL) {
+    fprintf(stderr, "curl_mime_addpart() failed\n");
+    return false;
   }
 
-  attempts = 0;
-  while ((res = curl_mime_name(field, "image")) != CURLE_OK &&
-         attempts < MAX_ATTEMPTS) {
-    fprintf(stderr,
-            "curl_mime_name() failed, attempt %d of %d: %s\n",
-            attempts + 1,
-            MAX_ATTEMPTS,
-            error);
-    attempts++;
+  res = curl_mime_name(field, "image");
+  if (res != CURLE_OK) {
+    fprintf(stderr, "curl_mime_name() failed\n");
+    return false;
   }
-  // std::cout << (*buf_ptr).size() << "\n";
-  attempts = 0;
-  while ((res = curl_mime_data(field,
-                               reinterpret_cast<const char *>(buf_ptr->data()),
-                               (*buf_ptr).size())) != CURLE_OK &&
-         attempts < MAX_ATTEMPTS) {
-    fprintf(stderr,
-            "curl_mime_filedata() failed, attempt %d of %d: %s\n",
-            attempts + 1,
-            MAX_ATTEMPTS,
-            error);
-    attempts++;
+  res = curl_mime_data(field,reinterpret_cast<const char *>(encoded->buf.data()), encoded->buf.size());
+  if (res != CURLE_OK) {
+    fprintf(stderr, "curl_mime_data() failed\n");
+    return false;
   }
 
   curl_mime_filename(field, timestamp_str.c_str());
@@ -214,22 +190,16 @@ bool HttpTransmitter::send_imen(const std::string &url,
   // timestamp_str = std::to_string(timestamp); curl_mime_data(field,
   // timestamp_str.c_str(), CURL_ZERO_TERMINATED);
 
-  attempts = 0;
-  while ((res = curl_easy_perform(curl)) != CURLE_OK &&
-         attempts < MAX_ATTEMPTS) {
-    fprintf(stderr,
-            "curl_easy_perform() failed, attempt %d of %d: %s\n",
-            attempts + 1,
-            MAX_ATTEMPTS,
-            error);
-    attempts++;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK) {
+    fprintf(stderr, "curl_mime_data() failed\n");
+    return false;
   }
 
   /* then cleanup the form */
   curl_mime_free(form);
 
-  std::cout << "Sent " << timestamp << " to " << url << '\n';
+  std::cout << "Sent " << encoded->timestamp << " to " << url << '\n';
 
   return true;
 }
---
+++ src/Pipeline.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+
+#ifndef PIPELINE_HPP
+#define PIPELINE_HPP
+
+struct ImagePath {
+  std::string path;
+  int64_t timestamp;
+};
+
+struct EncodedData {
+  std::vector<uchar> buf;
+  int64_t timestamp;
+};
+
+#endif // PIPELINE_HPP
