02f830fa2df39ff9ad4372def1154a2a51279786
Richard Han <rrhan2002@gmail.com>
2025-02-11 18:23:25 -0800

    opencl

--- src/curltest.cpp
+++ src/curltest.cpp
@@ -53,18 +53,17 @@ void demosaic_gpu() {
   cv::ocl::setUseOpenCL(true);
 
   // Load the image into UMat (which will use GPU memory)
-  cv::UMat im;
-  cv::imread("1918183719895.jpg", cv::IMREAD_COLOR).copyTo(im);
-
   cv::UMat mSource_Bayer;
+  cv::Mat bayer = cv::Mat::zeros(3648, 5472, CV_8UC1);
+  bayer.copyTo(mSource_Bayer);
 
-  cv::cvtColor(im, mSource_Bayer, cv::COLOR_BGR2GRAY);
 
   // Loop to continuously apply demosaic and convert Bayer to BGR
   while (1) {
     cv::UMat mSource_Bgr;
 
     cv::cvtColor(mSource_Bayer, mSource_Bgr, cv::COLOR_BayerRG2BGR);
+    std::cout << "HAHAHA\n";
 
     usleep(200000);  // Sleep for 0.2 seconds (200,000 microseconds)
   }
